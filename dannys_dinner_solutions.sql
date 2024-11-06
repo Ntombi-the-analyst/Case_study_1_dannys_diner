b0d6882aa817c4574460c6db6371f570aedbb5cb
Ntombi-the-analyst <khwelalungile1@gmail.com>
2024-11-06 20:44:04 +0200

    first question answered

--- dannys_dinner_solutions.sql
+++ dannys_dinner_solutions.sql
@@ -1,1 +1,11 @@
 select * from menu;
+
+-- 1. What is the total amount each customer spent at the restaurant?
+
+SELECT 
+A.customer_id,
+sum(B.price)
+from sales A
+inner join menu B
+on A.product_id= B.product_id
+group by A.customer_id;
